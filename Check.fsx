afe5ab28c15bbedd6dd29a87eae8b8d4ac55d183
StrigoEnTurbano <Strigo.En.Turbano@yandex.ru>
2024-03-05 00:00:14 +0500

    Перенес пример в публичный репозиторий.

---
+++ Check.fsx
@@ -0,0 +1,8 @@
+#load "Data/Stage1.Handmade.fs"
+#load "Data/Stage1.Generated.fs"
+#load "Data/Stage2.Handmade.fs"
+#load "Data/Stage2.Generated.fs"
+
+open PhotoTour
+
+Attractions.ДворецЗемледельцев.Card = AttractionCards.ДворецЗемледельцев
